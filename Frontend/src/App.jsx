b3709826f3084bd41c85a60e54b329e33a2e4a87
jis24 <165779138+orscoder@users.noreply.github.com>
2024-07-29 23:23:12 +0530

    name added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -32,4 +32,16 @@ function App() {
   )
 }
 
+function Navbar(){
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </nav>
+  )
+}
+
 export default App
+export { Navbar }
