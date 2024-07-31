84b4a621ca748bc0c1d3419cb824afc5fedd41e3
jis24 <165779138+orscoder@users.noreply.github.com>
2024-07-31 23:56:31 +0530

    cardstows added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css'
 import Videoplayer from './components/Videoplaer';
-
+import Cardstows from "./components/Cardstows";
 function App() {
  
   return  (
     <div>
       <Videoplayer />
+      <Cardstows/>
     </div>
   )
 };
---
+++ Frontend/src/components/Cardstows.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// import mainrevuelto from '/media/image/mainrevuelto';
+import r from "../../public/media/image/mainrevuelto.jpg"
+import './css/Videoplayer.css';
+function Cardstows(){
+    return(
+        <>
+    <div className="cardstow">
+    <img src={revuelto} alt="revuelto"/>
+    </div>
+         
+       </>
+    )
+};
+
+export default Cardstows;
