25fdcbdca1da92216afc884aa5d4989bfc187cb3
jis24 <165779138+orscoder@users.noreply.github.com>
2024-08-01 15:27:44 +0530

    text beyond img

--- Frontend/src/components/Cardstows.jsx
+++ Frontend/src/components/Cardstows.jsx
@@ -1,18 +1,17 @@
-// import React from "react";
-// import mainrevuelto from '/media/image/mainrevuelto';
-import revuelto from "../../public/media/image/mainrevuelto_removebg.png"
+import revuelto from "../../public/media/image/mainrevuelto-removebg.png"
 import './css/Cardstows.css';
 function Cardstows(){
     return(
         <>
     <div className="cardstow">
+        
+    
     <img src={revuelto} alt="revuelto" />
+    <div className="textonimage">
+    <p> REVUELTO</p>  
     </div>
-          <p>RACING TOGETHER TOWARDS SUCCESS</p>
-          <h1>MACRON X</h1>
-          <h1>AUTOMOBILI</h1>
-          <h1>LAMBORGHINI</h1>  
-       
+    </div>
+             
          
        </>
     )
